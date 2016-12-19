ae215bf4d4d9fbed12a2f49e9b4b2c37077b9677
mounaMT <mounatrabelsi071@gmail.com>
2016-12-19 01:15:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/htc/androidappmkit/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.htc.androidappmkit.app;
+
+/**
+ * Created by htc on 29/11/2016.
+ */
+
+public class AppConfig {
+    public static String URL_LOGIN = "http://localhost/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://localhost/android_login_api/register.php";
+}
